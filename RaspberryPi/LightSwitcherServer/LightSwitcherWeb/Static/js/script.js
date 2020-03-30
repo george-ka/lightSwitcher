6f420c8b26b109a4a510eb5ac5fc3ce5a173544c
savamura <georgekalashnikov@gmail.com>
2020-03-30 12:10:36 +0300

    Switches aligned with flexbox, switches now have clear labels and data-attributes

--- RaspberryPi/LightSwitcherServer/LightSwitcherWeb/Static/js/script.js
+++ RaspberryPi/LightSwitcherServer/LightSwitcherWeb/Static/js/script.js
@@ -1,4 +1,22 @@
-$(".colors span").click(function(){
-  color = $(this).attr("class");
-  $(".demo .minitoggle").removeAttr("class").addClass('minitoggle' + ' ' + color);
+var apiBaseUrl = "http://localhost:5000/api/v1/switches/";
+
+$('.toggle').minitoggle();
+$('.toggle').on("toggle", function(e)
+{
+	var switchId = $(this).data("switchid")
+	var commandUrl = apiBaseUrl + switchId + "?mode=" + (e.isActive ? "on" : "off");
+	
+	$.post(commandUrl, "", 
+		function(data, status, jqXHR)
+		{
+			console.log("request completed to " + commandUrl)
+			console.log(data)
+			console.log(status)
+		});
+
+	$("#status").html($(this).attr("id"))
+		if (e.isActive)
+		   ;// $("#status").html("Oh, you turn me on.")
+		else
+			$(".result").html("No, you turn me off.")
 });
